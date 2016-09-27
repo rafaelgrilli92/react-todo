8eb85cc2dbf86db7635bc15c97a360f35344bafd
Rafael Grilli <rafaelgrilli92@hotmail.com>
2016-09-27 18:21:05 +1000

    Add redux store

--- app/app.jsx
+++ app/app.jsx
@@ -1,8 +1,19 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var { Route, Router, IndexRoute, hashHistory } = require('react-router');
-var Main = require('Main');
+
 var TodoApp = require('TodoApp');
+
+var actions = require('actions');
+var store = require('configStore').config();
+
+store.subscribe(() => {
+	console.log('newState', store.getState());
+});
+
+store.dispatch(actions.addTodo('Study more'));
+store.dispatch(actions.setSearchText('more'));
+store.dispatch(actions.toggleShowCompleted());
 
 // Load Foundation
 $(document).foundation();
